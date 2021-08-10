2ceb6e74fb421931624e0614ab6e200f4567e226
Your Name <email@example.com>
2021-08-10 19:31:10 +0300

    Update

---
+++ Zadanie_3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Задание 3</title>
+</head>
+<body>
+<p>
+По сути результат от обоих запросов будет один и тот же а 
+отличие будет только в методе получения результата. Например,
+ Where будет фильтровать таблицы по критерию совпадения id, а 
+ Join объединяет данные таблиц по условию a.id=b.a_id;. Поэтому 
+ зачастую используют Join для связывания таблиц а where для фильтрации результата.
+
+</p>
+
+<br>
+<a href="Zadanie_4.php">Задание 4</a>
+</body>
+</html>
---
+++ Zadanie_4.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Задание 4</title>
+</head>
+<body>
+     <!-- Задание 4 -->
+    <?php
+$e = range(0,93); shuffle($e);
+$ar=[1,2,3,3,3,3,];
+$result=array_merge($e,$ar);
+$n = count ($result);
+
+
+function countCon($result, $n)
+{
+    $cnt = 0;
+    for ($i = 0; $i < $n - 1; $i++)
+    {
+        if ( $result[$i] == $result[$i + 1])
+            $cnt++;
+       
+    }
+    return $cnt;
+}
+
+echo'Найдено последовательных пар ',  countCon($result, $n);
+      ?>
+
+</body>
+</html>
